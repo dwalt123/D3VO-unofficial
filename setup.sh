cc1967e04034aa631205f1c592a3cb47e3403c7a
dwalt123 <walton.434@osu.edu>
2023-09-24 15:39:04 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Included in Python: 
+# os, sys, math, time, random, copy, __future__, collections
+
+#argparse
+pip3 install argparse
+
+#yaml
+pip3 install pyyaml
+
+#numpy
+pip3 install numpy
+
+#cv2
+pip3 install opencv-python
+
+#matplotlib
+pip3 install matplotlib
+
+# torch and torchvision
+pip3 install torch torchvision torchaudio
+
+#PIL
+pip3 install pillow
+
+#wandb
+pip3 install wandb
+
+#kornia
+pip3 install kornia
+
+#skimage
+pip3 install scikit-image
